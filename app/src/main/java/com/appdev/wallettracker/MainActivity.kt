3ed1edc0b65790f826b2dcd186d1e696ebb9e27d
mathewGlenn <glenngarmaappdev@gmail.com>
2022-11-04 20:41:52 +0800

    Add Account

--- app/src/main/java/com/appdev/wallettracker/MainActivity.kt
+++ app/src/main/java/com/appdev/wallettracker/MainActivity.kt
@@ -6,8 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +19,10 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
+
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var accountName: TextView = itemView.findViewById(R.id.vAccountName)
@@ -29,16 +33,25 @@ class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
 class MainActivity : AppCompatActivity() {
 
+
+
     private lateinit var accountsList: RecyclerView
     private lateinit var firestore: FirebaseFirestore
     private lateinit var entryAdapter: FirestoreRecyclerAdapter<Account, ViewHolder>
-
+lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+         binding = ActivityMainBinding.inflate(layoutInflater)
         val view: View = binding.root
         setContentView(view)
+
+        val formatter =
+            NumberFormat.getCurrencyInstance(Locale.US) as DecimalFormat
+        val symbols = formatter.decimalFormatSymbols
+        symbols.currencySymbol = "₱ "
+        formatter.decimalFormatSymbols = symbols
+
 
         accountsList = binding.accountRecyView
         firestore = FirebaseFirestore.getInstance()
@@ -53,41 +66,70 @@ class MainActivity : AppCompatActivity() {
             .setQuery(query, Account::class.java)
             .build()
 
+
         entryAdapter =
             object :
-            FirestoreRecyclerAdapter<Account, ViewHolder>(
-                accounts
-            ){
+                FirestoreRecyclerAdapter<Account, ViewHolder>(
+                    accounts
+                ) {
                 override fun onBindViewHolder(holder: ViewHolder, position: Int, account: Account) {
                     val name = account.name
                     val balance = account.balance
                     val color = account.color
-
                     holder.accountName.text = name
-                    holder.accountBalance.text = balance
+                    holder.accountBalance.text = formatter.format(balance.toFloat())
                     holder.accountColor.setCardBackgroundColor(Color.parseColor(color))
 
-                    holder.view.setOnClickListener{
-                        //
+                    val accountID = entryAdapter.snapshots.getSnapshot(position).id
+
+                    holder.view.setOnClickListener {
+                       val intent = Intent(applicationContext, ViewAccount::class.java)
+                        intent.putExtra("name", name)
+                        intent.putExtra("balance", balance)
+                        intent.putExtra("color", color)
+                        startActivity(intent)
                     }
                 }
+
                 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
                     val accountView: View =
                         LayoutInflater.from(parent.context)
                             .inflate(R.layout.card_account, parent, false)
+
                     return ViewHolder(accountView)
+                }
+
+                override fun onDataChanged() {
+                    super.onDataChanged()
+
+
+                    if (itemCount == 0){
+                        binding.totalBalance.text = "₱ 0.00"
+                    }else{
+                        query.get().addOnCompleteListener{
+                            if (it.isSuccessful){
+                                var total = 0.0f
+                                for (document in it.result) {
+                                    val totalBalance = document.getString("balance")!!.toFloat()
+                                    total += totalBalance
+                                }
+
+                                binding.totalBalance.text = formatter.format(total)
+                            }
+                        }
+                    }
                 }
             }
 
         accountsList.layoutManager = LinearLayoutManager(this)
         accountsList.adapter = entryAdapter
 
-        binding.btnAddAccount.setOnClickListener{
+        binding.btnAddAccount.setOnClickListener {
             startActivity(Intent(this, AddAccount::class.java))
         }
 
-        binding.menu.setOnClickListener{
-            startActivity(Intent(this, ViewAccount::class.java))
+        binding.menu.setOnClickListener {
+            Toast.makeText(this, "Coming soon", Toast.LENGTH_SHORT).show()
         }
 
     }
@@ -96,7 +138,9 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         entryAdapter.startListening()
+
     }
+
     override fun onStop() {
         super.onStop()
         entryAdapter.stopListening()
--- app/src/main/java/com/appdev/wallettracker/ViewAccount.kt
+++ app/src/main/java/com/appdev/wallettracker/ViewAccount.kt
@@ -1,10 +1,14 @@
 package com.appdev.wallettracker
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.appdev.wallettracker.databinding.ActivityViewAccountBinding
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
 
 class ViewAccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,20 @@ class ViewAccount : AppCompatActivity() {
         val binding = ActivityViewAccountBinding.inflate(layoutInflater)
         val view: View = binding.root
         setContentView(view)
+
+        val formatter =
+            NumberFormat.getCurrencyInstance(Locale.US) as DecimalFormat
+        val symbols = formatter.decimalFormatSymbols
+        symbols.currencySymbol = "₱ "
+        formatter.decimalFormatSymbols = symbols
+
+        val name = intent.getStringExtra("name")
+        val balance = intent.getStringExtra("balance")?.toFloat()
+        val color = intent.getStringExtra("color")
+
+        binding.cardBalance.setCardBackgroundColor(Color.parseColor(color))
+        binding.accountName.text = name.toString()
+        binding.totalBalance.text = formatter.format(balance)
 
         binding.edit.setOnClickListener{
             startActivity(Intent(this, EditAccount::class.java))
